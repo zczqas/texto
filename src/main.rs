daf057aa4a61448f21481ced7ca9a05532bd0ba4
zczqas <limbusanchar76@gmail.com>
2025-08-17 18:45:17 +0545

    Ctrl-q to quit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
+    // By using let editor... before, we told Rust that we want to have a read-only reference to Editor.
+    // However, editor.run() now modifies the Editor.
+    // We could make this an explicit reference that we intend to modify - or we could sidestep it by eliminating the variable editor and calling run() directly
+    // on the output of default().
 
-    editor.run();
+    // You can also store editor as a mutable reference
+    // let mut editor = Editor.default();
+    // editor.run();
+    Editor::default().run();
 }
