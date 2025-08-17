faba5d0fc028899eee5c5e9912c501e1d12bf4be
zczqas <limbusanchar76@gmail.com>
2025-08-17 10:54:04 +0545

    Add Error Handling

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use std::io::{self, Read};
 fn main() {
     enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
 
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
-
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => println!("Error: {}", err),
         }
     }
+    disable_raw_mode().unwrap();
 }
