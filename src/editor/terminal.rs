a62b460c9c1f41f3bf04bb619607f45324f416c2
zczqas <limbusanchar76@gmail.com>
2025-08-27 12:41:09 +0545

    Improved Lifecycle Management and Error Handling

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,6 +1,9 @@
 use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::style::Print;
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
+use crossterm::terminal::{
+    Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
+    enable_raw_mode, size,
+};
 use crossterm::{Command, queue};
 use std::io::{Error, Write, stdout};
 
@@ -23,12 +26,15 @@ pub struct Position {
 pub struct Terminal {}
 impl Terminal {
     pub fn terminate() -> Result<(), Error> {
+        Self::leave_alternate_screen()?;
+        Self::show_caret()?;
         Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
+        Self::enter_alternate_screen()?;
         Self::clear_screen()?;
         Ok(())
     }
@@ -48,6 +54,14 @@ impl Terminal {
         Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
         Ok(())
     }
+    pub fn enter_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(EnterAlternateScreen)?;
+        Ok(())
+    }
+    pub fn leave_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(LeaveAlternateScreen)?;
+        Ok(())
+    }
     pub fn hide_caret() -> Result<(), Error> {
         Self::queue_command(Hide)?;
         Ok(())
@@ -58,6 +72,12 @@ impl Terminal {
     }
     pub fn print(string: &str) -> Result<(), Error> {
         Self::queue_command(Print(string))?;
+        Ok(())
+    }
+    pub fn print_row(row: usize, line_text: &str) -> Result<(), Error> {
+        Self::move_caret_to(Position { row, col: 0 })?;
+        Self::clear_line()?;
+        Self::print(line_text)?;
         Ok(())
     }
     /// Returns the current size of this Terminal.
--- src/editor/view.rs
+++ src/editor/view.rs
@@ -1,6 +1,5 @@
-use super::terminal::{Position, Size, Terminal};
+use super::terminal::{Size, Terminal};
 use buffer::Buffer;
-use std::io::Error;
 mod buffer;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
@@ -17,20 +16,18 @@ impl View {
         self.size = to;
         self.needs_redraw = true;
     }
-    fn render_line(at: usize, line_text: &str) -> Result<(), Error> {
-        Terminal::move_caret_to(Position { row: at, col: 0 })?;
-        Terminal::clear_line()?;
-        Terminal::print(line_text)?;
-        Ok(())
+    fn render_line(at: usize, line_text: &str) {
+        let result = Terminal::print_row(at, line_text);
+        debug_assert!(result.is_ok(), "Failed to render line");
     }
-    pub fn render(&mut self) -> Result<(), Error> {
+    pub fn render(&mut self) {
         if !self.needs_redraw {
-            return Ok(());
+            return;
         }
 
         let Size { height, width } = self.size;
         if height == 0 || width == 0 {
-            return Ok(());
+            return;
         }
 
         // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
@@ -45,15 +42,14 @@ impl View {
                 } else {
                     line
                 };
-                Self::render_line(current_row, truncated_line)?;
+                Self::render_line(current_row, truncated_line);
             } else if current_row == vertical_center && self.buffer.is_empty() {
-                Self::render_line(current_row, &Self::build_welcome_message(width))?;
+                Self::render_line(current_row, &Self::build_welcome_message(width));
             } else {
-                Self::render_line(current_row, "~")?;
+                Self::render_line(current_row, "~");
             }
         }
         self.needs_redraw = false;
-        Ok(())
     }
     fn build_welcome_message(width: usize) -> String {
         if width <= 0 {
