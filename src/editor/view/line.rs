206ec3ac12729b01838684be872f10378c3077a6
zczqas <limbusanchar76@gmail.com>
2025-09-01 15:20:15 +0545

    Proper Grapheme Handling, Pt 1

--- src/editor/view/line.rs
+++ src/editor/view/line.rs
@@ -1,5 +1,7 @@
 use std::{cmp, ops::Range};
 
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct Line {
     string: String,
 }
@@ -13,9 +15,13 @@ impl Line {
     pub fn get(&self, range: Range<usize>) -> String {
         let start = range.start;
         let end = cmp::min(range.end, self.string.len());
-        self.string.get(start..end).unwrap_or_default().to_string()
+        self.string
+            .graphemes(true)
+            .skip(start)
+            .take(end.saturating_sub(start))
+            .collect()
     }
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.string[..].graphemes(true).count()
     }
 }
