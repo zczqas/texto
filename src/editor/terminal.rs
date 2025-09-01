387281d73e76527385bf23be506e1592c1903eaa
zczqas <limbusanchar76@gmail.com>
2025-09-01 16:02:51 +0545

    Support Character Width
    - proper support for characters of different widths

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -17,6 +17,15 @@ pub struct Position {
     pub col: usize,
     pub row: usize,
 }
+
+impl Position {
+    pub const fn saturating_sub(self, other: Self) -> Self {
+        Self {
+            row: self.row.saturating_sub(other.row),
+            col: self.col.saturating_sub(other.col),
+        }
+    }
+}
 /// Represents the Terminal.
 /// Edge Case for platforms where `usize` < `u16`:
 /// Regardless of the actual size of the Terminal, this representation
