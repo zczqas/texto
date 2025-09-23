0499c8d5e266bc1ec9a0f216ced516ea54439195
Sanchar Limbu <limbusanchar76@gmail.com>
2025-09-23 20:35:00 +0545

    Colourful Multi Line Comments

--- src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
+++ src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
@@ -1,6 +1,5 @@
 use super::{Annotation, AnnotationType, Line, SyntaxHighlighter};
 use crate::prelude::*;
-use std::collections::HashMap;
 use unicode_segmentation::UnicodeSegmentation;
 
 const KEYWORDS: [&str; 52] = [
@@ -66,16 +65,55 @@ const KNOWN_VALUES: [&str; 6] = ["Some", "None", "true", "false", "Ok", "Err"];
 
 #[derive(Default)]
 pub struct RustSyntaxHighlighter {
-    highlights: HashMap<LineIdx, Vec<Annotation>>,
+    highlights: Vec<Vec<Annotation>>,
+    ml_comment_balance: usize,
 }
+impl RustSyntaxHighlighter {
+    fn annotate_ml_comment(&mut self, string: &str) -> Option<Annotation> {
+        let mut chars = string.char_indices().peekable();
 
+        while let Some((_, char)) = chars.next() {
+            if char == '/' {
+                //check for a ml comment opener
+                if let Some((_, '*')) = chars.peek() {
+                    self.ml_comment_balance = self.ml_comment_balance.saturating_add(1);
+                    chars.next();
+                }
+            } else if self.ml_comment_balance == 0 {
+                return None; // We saw no opener, and we are not currently in a multi-line comment, returning None
+            } else if char == '*' {
+                if let Some((idx, '/')) = chars.peek() {
+                    self.ml_comment_balance = self.ml_comment_balance.saturating_sub(1);
+
+                    if self.ml_comment_balance == 0 {
+                        return Some(Annotation {
+                            annotation_type: AnnotationType::Comment,
+                            start: 0,
+                            end: idx.saturating_add(1),
+                        });
+                    }
+                    chars.next();
+                }
+            }
+        }
+
+        (self.ml_comment_balance > 0).then_some(Annotation {
+            annotation_type: AnnotationType::Comment,
+            start: 0,
+            end: string.len(),
+        })
+    }
+}
 impl SyntaxHighlighter for RustSyntaxHighlighter {
     fn highlight(&mut self, idx: LineIdx, line: &Line) {
+        debug_assert_eq!(idx, self.highlights.len());
         let mut result = Vec::new();
         let mut iterator = line.split_word_bound_indices().peekable();
         while let Some((start_idx, _)) = iterator.next() {
             let remainder = &line[start_idx..];
-            if let Some(mut annotation) = annotate_single_line_comment(remainder)
+            if let Some(mut annotation) = self
+                .annotate_ml_comment(remainder)
+                .or_else(|| annotate_single_line_comment(remainder))
                 .or_else(|| annotate_char(remainder))
                 .or_else(|| annotate_lifetime_specifier(remainder))
                 .or_else(|| annotate_number(remainder))
@@ -84,6 +122,7 @@ impl SyntaxHighlighter for RustSyntaxHighlighter {
                 .or_else(|| annotate_known_value(remainder))
             {
                 annotation.shift(start_idx);
+
                 result.push(annotation);
                 // Skip over any subsequent word which has already been annotated in this step
                 while let Some(&(next_idx, _)) = iterator.peek() {
@@ -94,10 +133,10 @@ impl SyntaxHighlighter for RustSyntaxHighlighter {
                 }
             };
         }
-        self.highlights.insert(idx, result);
+        self.highlights.push(result);
     }
     fn get_annotations(&self, idx: LineIdx) -> Option<&Vec<Annotation>> {
-        self.highlights.get(&idx)
+        self.highlights.get(idx)
     }
 }
 
@@ -187,7 +226,6 @@ fn is_valid_number(word: &str) -> bool {
     if is_numeric_literal(word) {
         return true;
     }
-
     let mut chars = word.chars();
 
     // Check the first character
@@ -254,13 +292,13 @@ fn is_numeric_literal(word: &str) -> bool {
     };
     chars.all(|char| char.is_digit(base))
 }
-
 fn is_keyword(word: &str) -> bool {
     KEYWORDS.contains(&word)
 }
 fn is_type(word: &str) -> bool {
     TYPES.contains(&word)
 }
+
 fn is_known_value(word: &str) -> bool {
     KNOWN_VALUES.contains(&word)
 }
