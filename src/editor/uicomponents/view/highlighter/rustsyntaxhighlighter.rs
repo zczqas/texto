9a8a0f392f71f350736604dba3fd03b9d3b4581d
Sanchar Limbu <limbusanchar76@gmail.com>
2025-09-23 20:31:37 +0545

    Colourful single line comments

--- src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
+++ src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
@@ -75,7 +75,8 @@ impl SyntaxHighlighter for RustSyntaxHighlighter {
         let mut iterator = line.split_word_bound_indices().peekable();
         while let Some((start_idx, _)) = iterator.next() {
             let remainder = &line[start_idx..];
-            if let Some(mut annotation) = annotate_char(remainder)
+            if let Some(mut annotation) = annotate_single_line_comment(remainder)
+                .or_else(|| annotate_char(remainder))
                 .or_else(|| annotate_lifetime_specifier(remainder))
                 .or_else(|| annotate_number(remainder))
                 .or_else(|| annotate_keyword(remainder))
@@ -164,6 +165,17 @@ fn annotate_lifetime_specifier(string: &str) -> Option<Annotation> {
                 end: idx.saturating_add(next_word.len()),
             });
         }
+    }
+    None
+}
+
+fn annotate_single_line_comment(string: &str) -> Option<Annotation> {
+    if string.starts_with("//") {
+        return Some(Annotation {
+            annotation_type: AnnotationType::Comment,
+            start: 0,
+            end: string.len(),
+        });
     }
     None
 }
